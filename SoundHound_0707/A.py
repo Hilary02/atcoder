da9b4faa0cd5d13fc161cbffae50a50b9302f808
Hilary <c011605154@edu.teu.ac.jp>
2018-10-30 11:52:55 +0900

    以前のものを追加

---
+++ SoundHound_0707/A.py
@@ -0,0 +1,9 @@
+a,b = input().split(" ")
+a = int(a)
+b = int(b)
+if a+b ==15:
+    print("+")
+elif a*b == 15:
+    print("*")
+else:
+    print("x")
---
+++ SoundHound_0707/B.py
@@ -0,0 +1,6 @@
+str = input()
+w = int(input())
+
+ans =str[::w]
+
+print(ans)
