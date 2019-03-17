ee5cadbba3fcf183d32a09b46ffc228b645b6fc4
Hilary <c011605154@edu.teu.ac.jp>
2019-03-17 14:26:41 +0900

    mod m.bat

--- m.bat
+++ m.bat
@@ -1,2 +1,10 @@
-@g++ -O3 -mtune=native -march=native -mfpmath=both %1
+@echo off
+IF EXIST a.exe (GOTO FILE_TRUE) ELSE GOTO COMPILE
+
+:FILE_TRUE
+del a.exe
+GOTO :compile
+
+:COMPILE
+g++ -O3 -mtune=native -march=native -mfpmath=both %1
 call a.exe
